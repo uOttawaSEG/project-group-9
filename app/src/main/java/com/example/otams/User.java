2bf759fc20721da4fa7b30df658b23995c5624ba
Sophia Hopkins <sophiagchopkins@gmail.com>
2025-10-03 12:24:45 -0400

    Added almost variables needed to User Class

---
+++ app/src/main/java/com/example/otams/User.java
@@ -0,0 +1,11 @@
+package com.example.otams;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int phoneNumber;
+    private String email;
+
+
+}
