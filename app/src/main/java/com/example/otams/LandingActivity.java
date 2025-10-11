ca8d81ea9d23d9a78722f4468afa8d1e45ffb548
SayohnDavid <sayohn135@gmail.com>
2025-10-11 13:45:12 -0400

    Updated main, manifest, gradle, and other UI files except tutor layout

--- app/src/main/java/com/example/otams/LandingActivity.java
+++ app/src/main/java/com/example/otams/LandingActivity.java
@@ -1,26 +1,29 @@
 package com.example.otams;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity {
+
+    Button btnGetStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_landing);
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-         */
+        btnGetStarted = findViewById(R.id.getStarted);
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LandingActivity.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
