7e227b6a1202dbc753f1f482fff5b0af1ebace2b
Onur <onur.onel2230@gmail.com>
2025-10-25 18:39:01 -0400

    Updates

--- app/src/main/java/com/example/otams/Administrator.java
+++ app/src/main/java/com/example/otams/Administrator.java
@@ -1,8 +1,69 @@
 package com.example.otams;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/** Administrator class
+ *
+ * @author Lauren Hendley [lhend093@uottawa.ca]
+ */
 public class Administrator extends User {
-    @Override
-    public void approveRegistration(String userEmail) {
-        super.approveRegistration(userEmail);
+    // Instantiating variables
+    private final List<Request> requests = new ArrayList<>();
+    private String email;
+    private String password;
+
+    /** Constructor for the administrator object
+     * @param email
+     */
+    public Administrator(String email){
+        super(email);
+    }
+
+    /** [GETTER] Returns the list of requests
+     * @return list of requests
+     */
+    public List<Request> getRequests(){
+        return this.requests;
+    }
+
+    /** Approves all the requests in the list of requests
+     */
+    public void approveAll(){
+        for(Request r : requests){
+            r.setApprovalApproved();
+        }
+    }
+
+    /** Denys all the requests in the list of requests
+     */
+    public void denyAll(){
+        for(Request r : requests){
+            r.setApprovalNotApproved();
+        }
+    }
+
+    /** Approves a certain request under a user's email
+     * @param userEmail
+     */
+    public void approve(String userEmail){
+        for(Request r : requests){
+            if(r.getUser().getEmail() == userEmail){
+                r.setApprovalApproved();
+                return;
+            }
+        }
+    }
+
+    /** Denies a certain request under a user's email
+     * @param userEmail
+     */
+    public void deny(String userEmail){
+        for(Request r : requests){
+            if(r.getUser().getEmail() == userEmail){
+                r.setApprovalNotApproved();
+                return;
+            }
+        }
     }
 }
