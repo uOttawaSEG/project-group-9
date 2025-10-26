17fadf076be05daf0c0e5cc36dae782e20d7fd95
NarymanH <naryman.hussein@gmail.com>
2025-10-26 13:08:04 -0400

    Fixed id names of the regreg and rejecreq buttons

--- app/src/main/java/com/example/otams/RejectedRequestsPage.java
+++ app/src/main/java/com/example/otams/RejectedRequestsPage.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class InboxAdminApprovalPage extends AppCompatActivity {
+public class RejectedRequestsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_inbox_admin_approval_page);
+        setContentView(R.layout.activity_inbox_rejection_request_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
