a1d89dbd189f5cd0236d238723e368c8dfc6f7ef
imanemm <imanemoussamarou@gmail.com>
2025-10-31 15:38:39 -0400

    Add Tutor Home layouts and cards for Availability, Requests, and Sessions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
+    implementation(libs.viewpager2)
+    implementation("com.google.android.material:material:1.12.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
 }
 
 
