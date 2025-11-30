a597040a3cc940b03dafa92874ea338d22512a83
Sophia Hopkins <sophiagchopkins@gmail.com>
2025-11-30 10:51:45 -0500

    checking to see if circleci is working

--- app/src/test/java/com/example/otams/ExampleUnitTest.java
+++ app/src/test/java/com/example/otams/ExampleUnitTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    //checks if average is calculated correctly
     @Test
     public void average_multipleRatings_computesCorrectly() {
         Tutor tutor = new Tutor();
